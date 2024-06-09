1ced7fdec71f223bf1521457360a41714ec6c3c6
Astrorick <me@astrorick.space>
2024-06-09 19:12:38 +0200

    Add user count display to server

--- internal/seekret/server.go
+++ internal/seekret/server.go
@@ -4,6 +4,7 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
+	"time"
 
 	"github.com/astrorick/seekret/pkg/version"
 	_ "github.com/mattn/go-sqlite3"
@@ -18,7 +19,7 @@ func NewServer(configPath string) *Server {
 	// define server version
 	serverVersion := &version.Version{
 		Major: 0,
-		Minor: 7,
+		Minor: 10,
 		Patch: 0,
 	}
 
@@ -44,23 +45,22 @@ func (s *Server) Start() error {
 
 	// print banner and welcome string
 	fmt.Println(serverBanner)
-	fmt.Printf("Seekret Server v%s by Astrorick\n\n", s.ServerVersion)
-
-	s.PrintServerConfig()
+	fmt.Printf("Seekret Server v%s by Astrorick\n", s.ServerVersion)
+	fmt.Printf("Current server datetime is %s\n\n", time.Now().Format("2006-01-02 15:04:05"))
 
 	// initialize database object
-	db, err := sql.Open("sqlite3", s.ServerConfig.DatabaseConnStr)
+	db, err := sql.Open(s.ServerConfig.DatabaseType, s.ServerConfig.DatabaseConnStr)
 	if err != nil {
 		return err
 	}
 	defer db.Close()
 
-	// ping database
-	if err := db.Ping(); err != nil {
+	// enum users in database (also checks if database is ok)
+	var userCount int
+	if err := db.QueryRow("SELECT COUNT(*) FROM users").Scan(&userCount); err != nil {
 		return err
 	}
-
-	fmt.Println("DB OK")
+	fmt.Printf("Successfully loaded '%s': %d registered users.", s.ServerConfig.DatabaseConnStr, userCount)
 
 	// exit
 	return nil
