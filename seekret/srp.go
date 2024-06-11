1d6aed0f9067faec4efc5caa8619387841ec2375
Astrorick <me@astrorick.space>
2024-06-11 18:23:13 +0200

    SRP client implementation WIP

--- seekret/srp.go
+++ seekret/srp.go
@@ -2,12 +2,29 @@ package seekret
 
 import "math/big"
 
+type SRPParams struct {
+	// TODO
+	params string
+}
+
 type SRPClient struct {
-	username string
-	salt     []byte
+	Params   *SRPParams
+	Username string
+	Password string
 	a        *big.Int
 	A        *big.Int
-	B        *big.Int
+}
+
+func NewClient(params *SRPParams, username string, password string, salt []byte) (*SRPClient, error) {
+	// TODO: compute 'a' and 'A'
+
+	return &SRPClient{
+		Params:   params,
+		Username: username,
+		Password: password,
+		a:        &big.Int{},
+		A:        &big.Int{},
+	}, nil
 }
 
 type SRPServer struct {
